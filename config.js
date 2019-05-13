261ea1c221979a156ba353197cf706fea39d8114
Daron McIntosh <mcintoshdaron@gmail.com>
2019-05-13 18:22:19 -0400

    add isProduction and set default level for logger

--- config.js
+++ config.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
+const log = require('loglevel');
 
-// const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
+
+if (!isProduction) {
+  log.setDefaultLevel('TRACE');
+} else {
+  log.setDefaultLevel('INFO');
+}
 
 module.exports = {
   apiKey: process.env.API_KEY,
+  isProduction,
 };
